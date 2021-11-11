f9c63f08a17925f2ae3d281c2a7688ae134e7d67
Sascha Zieger <sascha@zieger.dev>
2021-11-11 13:51:54 +0100

    fix

--- applyTag.sh
+++ applyTag.sh
@@ -7,3 +7,4 @@ mv desktop/build.gradle2.kts desktop/build.gradle.kts
 
 sed -E 's/const val currentTag = "v[0-9]+\.[0-9]+\.[0-9]+"/const val currentTag = "'$1'"/g' common/src/commonMain/kotlin/dev/zieger/mpchatdemo/common/CurrentTag.kt > common/src/commonMain/kotlin/dev/zieger/mpchatdemo/common/CurrentTag2.kt
 mv common/src/commonMain/kotlin/dev/zieger/mpchatdemo/common/CurrentTag2.kt common/src/commonMain/kotlin/dev/zieger/mpchatdemo/common/CurrentTag.kt
+
